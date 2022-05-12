6df19a13f480b0042df49093bbd852c76ac4a42a
Oleh Sivak <olegsivak.2003@gmail.com>
2022-05-12 23:10:03 +0300

    Add image model

---
+++ ImageSearch/Models/Image.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ImageSearch.Models
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
