899b77afe1f1f50b059a9bfabead12b7ea3387dd
Oleh Sivak <olegsivak.2003@gmail.com>
2022-05-12 23:10:10 +0300

    Add admin model

---
+++ ImageSearch/Models/Admin.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageSearch.Models
+{
+    public class Admin
+    {
+        public static string AccessPassword = "3AccessAdmin0";
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
