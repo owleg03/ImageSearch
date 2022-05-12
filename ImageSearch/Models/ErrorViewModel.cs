be61f36ea3a0ea3f61f03ddb999dfc7dcf45d01c
Oleh Sivak <olegsivak.2003@gmail.com>
2022-05-12 22:49:52 +0300

    Add template solution

---
+++ ImageSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
