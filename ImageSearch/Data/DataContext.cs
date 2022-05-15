45cab0ea1820c22183fbfb957f7959b0ff1b1c76
Oleh Sivak <olegsivak.2003@gmail.com>
2022-05-15 14:04:55 +0300

    Add data context

---
+++ ImageSearch/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ImageSearch.Models;
+
+namespace ImageSearch.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) 
+        {
+            Database.EnsureCreated();    
+        }
+        public DbSet<Admin> Admins;
+        public DbSet<Image> Images;
+    }
+}
