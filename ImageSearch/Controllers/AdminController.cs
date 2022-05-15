9c97021f61502099099dc9a79105e529a9bd3fa8
Oleh Sivak <olegsivak.2003@gmail.com>
2022-05-15 20:19:58 +0300

    Add login controller action

--- ImageSearch/Controllers/AdminController.cs
+++ ImageSearch/Controllers/AdminController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using ImageSearch.Data;
 using ImageSearch.Models;
@@ -22,9 +23,10 @@ namespace ImageSearch.Controllers
         // GET: Admins
         public async Task<IActionResult> Index()
         {
-              return _context.Admins != null ? 
-                          View(await _context.Admins.ToListAsync()) :
-                          Problem("Entity set 'DataContext.Admins'  is null.");
+            //ViewBag.LoggedInIndex = -1;
+            return _context.Admins != null ? 
+                   View(await _context.Admins.ToListAsync()) :
+                   Problem("Entity set 'DataContext.Admins'  is null.");
         }
 
         // GET: Admins/Details/5
@@ -113,7 +115,7 @@ namespace ImageSearch.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("IndexLoggedIn", new { id = id });
             }
             return View(admin);
         }
@@ -158,6 +160,52 @@ namespace ImageSearch.Controllers
         private bool AdminExists(int id)
         {
           return (_context.Admins?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        public async Task<IActionResult> LogIn(int? id)
+        {
+            if (id == null || _context.Admins == null)
+            {
+                return NotFound();
+            }
+
+            var admin = await _context.Admins.FindAsync(id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            admin.Password = "";
+            return View(admin);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogIn(int id, [Bind("Id,Login,Password")] Admin admin)
+        {
+            if (id != admin.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var adminFromContext = await _context.Admins.FindAsync(id);
+                if (admin.Password == adminFromContext.Password)
+                {
+                    return RedirectToAction("IndexLoggedIn", new { id = id });
+                }
+                ViewBag.IsCorrectPassword = false;
+                return View(admin);
+            }
+            return View(admin);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> IndexLoggedIn(int id)
+        {
+            ViewBag.LoggedInId = id;
+            return View("Index", await _context.Admins.ToListAsync());
         }
     }
 }
