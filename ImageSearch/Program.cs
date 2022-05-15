ec17bc59914098d1015af79f4dbe56390d06e34d
Oleh Sivak <olegsivak.2003@gmail.com>
2022-05-15 14:06:48 +0300

    Add context to builer services

--- ImageSearch/Program.cs
+++ ImageSearch/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ImageSearch.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
